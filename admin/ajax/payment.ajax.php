89844cc792084a307790f06afe9d0530e6b8ac4d
amitjamwal85@gmail.com <amitjamwal85@gmail.com>
2020-09-24 14:58:52 +0530

    payment admin

---
+++ admin/ajax/payment.ajax.php
@@ -0,0 +1,82 @@
+<?php
+
+// includes and security
+include_once('../_local_auth.inc.php');
+
+$iDisplayLength = (int) $_REQUEST['iDisplayLength'];
+$iDisplayStart  = (int) $_REQUEST['iDisplayStart'];
+$sSortDir_0     = $_REQUEST['sSortDir_0'] ? $_REQUEST['sSortDir_0'] : "desc";
+$filterText     = $_REQUEST['filterText'] ? $_REQUEST['filterText'] : null;
+$filterUserId   = $_REQUEST['filterUserId'] ? $_REQUEST['filterUserId'] : null;
+
+// get sorting columns
+$iSortCol_0     = (int) $_REQUEST['iSortCol_0'];
+$sColumns       = trim($_REQUEST['sColumns']);
+$arrCols        = explode(",", $sColumns);
+$sortColumnName = $arrCols[$iSortCol_0];
+$sort           = 'date_created';
+switch ($sortColumnName)
+{
+    case 'payment_date':
+        $sort = 'created_at';
+        break;
+    case 'user_name':
+        $sort = 'users.username';
+        break;
+    case 'amount':
+        $sort = 'amount';
+        break;
+}
+
+$sqlClause = "WHERE 1=1 ";
+if ($filterText)
+{
+    $filterText = $db->escape($filterText);
+    $sqlClause .= "AND (users.username LIKE '%" . $filterText . "%' OR ";
+    $sqlClause .= "description LIKE '%" . $filterText . "%' OR ";
+    $sqlClause .= "from_email LIKE '%" . $filterText . "%')";
+}
+
+if (strlen($filterByUser))
+{
+    $sqlClause .= " AND user_id = " . (int)$filterByUser;
+}
+
+if(strlen($filterUserId))
+{
+    $sqlClause .= " AND user_id = " . (int)$filterUserId;
+}
+
+$totalRS   = $db->getValue("SELECT COUNT(payment_laser.pay_id) AS total FROM payment_laser");
+$limitedRS = $db->getRows("SELECT payment_laser.pay_id, payment_laser.created_at, payment_laser.order_id, payment_laser.payment_id FROM payment_laser");
+
+$data = array();
+if (COUNT($limitedRS) > 0)
+{
+    foreach ($limitedRS AS $row)
+    {
+        $lRow = array();
+        $icon        = 'assets/images/icons/system/16x16/process.png';
+        $lRow[]      = '<img src="' . $icon . '" width="16" height="16" title="payment" alt="payment"/>';
+        $lRow[]      = coreFunctions::formatDate($row['date_created'], SITE_CONFIG_DATE_TIME_FORMAT);
+        // $lRow[]      = '<a href="user_manage.php?filterByAccountId='.urlencode($row['user_id']).'">'.adminFunctions::makeSafe($row['username']).'</a>';
+        $lRow[]      = adminFunctions::makeSafe("dummy_user");
+        $lRow[]      = adminFunctions::makeSafe($row['order_id']);
+        $lRow[]      = adminFunctions::makeSafe($row['payment_id']);
+        $lRow[]      = adminFunctions::makeSafe("dummy_status");
+
+        $links = array();
+        $links[] = '<a href="#" class="btn btn-default btn-sm" data-toggle="tooltip" data-placement="top" data-original-title="view" onClick="viewPaymentDetail(' . (int) $row['id'] . '); return false;"><span class="fa fa-info text-primary" aria-hidden="true"></span></a>';
+        $lRow[] = '<div class="btn-group">'.implode(" ", $links).'</div>';
+
+        $data[] = $lRow;
+    }
+}
+
+$resultArr = array();
+$resultArr["sEcho"]                = intval($_GET['sEcho']);
+$resultArr["iTotalRecords"]        = (int) $totalRS;
+$resultArr["iTotalDisplayRecords"] = $resultArr["iTotalRecords"];
+$resultArr["aaData"]               = $data;
+
+echo json_encode($resultArr);
